78c23b2c02a1727df4f2215204abb7b9e06fac0c
Aaron Katz <akatz@akamai.com>
2015-12-16 16:00:01 -0500

    somehow made git think I deleted these files... re-adding with code review fixes

---
+++ src/main/java/com/contrastsecurity/rO0/TestCases/BlacklistElement.java
@@ -0,0 +1,12 @@
+package com.contrastsecurity.rO0.TestCases;
+
+import java.io.Serializable;
+
+public class BlacklistElement implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/com/contrastsecurity/rO0/TestCases/WhitelistElement.java
@@ -0,0 +1,12 @@
+package com.contrastsecurity.rO0.TestCases;
+
+import java.io.Serializable;
+
+public class WhitelistElement implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
