220b73153792d3c44f4b6053b8c87202366d27c4
Aaron Katz <akatz@akamai.com>
2015-12-17 17:17:49 -0500

    Remove explisit references to my home directory and fix error executing TestSafeObjectInputStream tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "moving to ./bin"
 pushd bin
 
@@ -10,9 +12,10 @@ jar cfm contrast-ro0.jar  ../mf/contrast.mf com/contrastsecurity/rO0/*.class
 jar cfm contrast-ro0-spotfix.jar ../mf/safeois.mf com/akamai/security/*.class
 
 echo "executing tests"
-java -javaagent:/Users/akatz/git/rO0/contrast-ro0/bin/contrast-ro0.jar -Xbootclasspath/p:"/Users/akatz/git/rO0/contrast-ro0/bin/contrast-rO0.jar:/Users/akatz/git/rO0/contrast-ro0/lib/asm-5.0.4.jar:/Users/akatz/git/rO0/contrast-ro0/lib/asm-commons-5.0.4.jar" -jar contrast-test.jar -Dfile.encoding=UTF-8 -classpath /Users/akatz/git/rO0/contrast-ro0/bin:/Users/akatz/git/rO0/contrast-ro0/lib/asm-5.0.4.jar:/Users/akatz/git/rO0/contrast-ro0/lib/asm-commons-5.0.4.jar
+export JARDIR=`pwd`
+java -javaagent:${JARDIR}/contrast-ro0.jar -Xbootclasspath/p:"${JARDIR}/contrast-rO0.jar:${JARDIR}/asm-5.0.4.jar:${JARDIR}/asm-commons-5.0.4.jar" -jar contrast-test.jar -Dfile.encoding=UTF-8 -classpath ${JARDIR}/asm-5.0.4.jar:${JARDIR}/asm-commons-5.0.4.jar
 
-java -jar contrast-ro0-spotfix.jar -classpath:contrast-ro0.jar
+java com/akamai/security/TestSafeObjectInputStream
 
 # clean up 
 popd
